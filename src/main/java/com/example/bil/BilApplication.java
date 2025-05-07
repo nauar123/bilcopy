0b34054990a001bf8da2915d627f04216932d556
Badevand <mu4maroc@gmail.com>
2025-05-07 10:03:18 +0200

    first commit test

---
+++ src/main/java/com/example/bil/BilApplication.java
@@ -0,0 +1,13 @@
+package com.example.bil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bil/BilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
