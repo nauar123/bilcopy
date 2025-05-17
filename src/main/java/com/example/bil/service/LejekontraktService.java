9934254ddab3ef1a63a2d18bbb7ca0e6ef3acc1d
nauar123 <nauarbahaa@gmail.com>
2025-05-17 17:18:53 +0200

    har lavet service klassen til lejekontrakt

--- src/main/java/com/example/bil/service/LejekontraktService.java
+++ src/main/java/com/example/bil/service/LejekontraktService.java
@@ -1,5 +1,31 @@
 package com.example.bil.service;
 
+import com.example.bil.model.Lejekontrakt;
+import com.example.bil.repository.LejekontraktRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+
+import java.util.List;
+
+
+@Repository
+
+
 public class LejekontraktService {
+    @Autowired
+    LejekontraktRepo lejekontraktRepo;
+
+
+
+
+    public List<Lejekontrakt> fetchAll() {
+        return lejekontraktRepo.fetchAll();
+    }
+
+
+    public void addLejekontrakt(Lejekontrakt l)
+    {
+        lejekontraktRepo.addLejekontrakt(l);
+    }
 }
-//a
