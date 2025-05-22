3ed8dd4b6ac74c9664b5018eb31e2098bfaa5a10
nauar123 <nauarbahaa@gmail.com>
2025-05-22 16:48:22 +0200

    har skrevet et par noter og rettet til

--- src/main/java/com/example/bil/service/KundeService.java
+++ src/main/java/com/example/bil/service/KundeService.java
@@ -1,5 +1,42 @@
 package com.example.bil.service;
 
-public class KundeService {
-}
-//a
+import com.example.bil.model.Kunde;
+import com.example.bil.repository.KundeRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+    @Service
+    public class KundeService {
+
+        @Autowired
+        private KundeRepo kundeRepo;
+
+        // Henter alle kunder
+        public List<Kunde> fetchAll() {
+            return kundeRepo.fetchAll();
+        }
+
+        // Finder kunde efter ID
+        public Kunde findKundeById(int kundeId) {
+            return kundeRepo.findById(kundeId);
+        }
+
+        // Opretter ny kunde
+        public void opretKunde(Kunde kunde) {
+            kundeRepo.addKunde(kunde);
+        }
+
+        // Opdaterer eksisterende kunde
+        public void opdaterKunde(Kunde kunde) {
+            kundeRepo.updateKunde(kunde);
+        }
+
+        // Sletter kunde
+        public void sletKunde(int kundeId) {
+            kundeRepo.deleteById(kundeId);
+        }
+    }
+
+
