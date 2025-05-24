bd48c75e0a366ff1806cc298931b80a199244a69
Badevand <mu4maroc@gmail.com>
2025-05-24 23:15:45 +0200

    a

--- src/main/java/com/example/bil/service/BilService.java
+++ src/main/java/com/example/bil/service/BilService.java
@@ -1,7 +1,6 @@
 package com.example.bil.service;
 
 import com.example.bil.model.Bil;
-import com.example.bil.repository.BilRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
