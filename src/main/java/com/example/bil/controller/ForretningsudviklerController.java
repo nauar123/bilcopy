ca24c0806ec20c9917f983bc25959c573e9ee7e7
nauar123 <nauarbahaa@gmail.com>
2025-05-27 13:49:10 +0200

    azure link tilføjet

--- src/main/java/com/example/bil/controller/ForretningsudviklerController.java
+++ src/main/java/com/example/bil/controller/ForretningsudviklerController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Controller
 public class ForretningsudviklerController {
-
+//nn
     //       ---- FORRETNINGSUDVIKLER ----
     @Autowired
     protected ForretningsudviklerService forretningsudviklerService;
